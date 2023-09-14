726c1579aec0673586929d6c4631543e7bbf1e86
19633 <ziyoeng_zoeng@foxmail.com>
2023-09-14 22:45:31 +0800

    新版本2.1.0，具体修改详见README.md

--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(
     include_package_data=True,
     python_requires='>=3.6',
     install_requires=['numpy>=1.24.2', 'onnx>=1.13.1', 'onnx_tf>=1.10.0', 'onnxruntime-gpu', 'setuptools>=56.0.0', 'tensorflow>=2.11.0',
-                      'tensorflow_intel>=2.11.0', 'tensorflow-gpu>=2.12.0', 'tf2onnx>=1.13.0', 'torch>=1.11.0', 'torchvision>=0.15.2', 'tensorflow_probability'],
+                      'tensorflow-gpu>=2.12.0', 'tf2onnx>=1.13.0', 'torch>=1.11.0', 'torchvision>=0.15.2', 'tensorflow_probability'],
     url='https://github.com/1963306815/shengtengconverter'
 )
